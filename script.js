1836275e260ec16621c9de9de8204109a663e85e
atatm <alelignayana@gmail.com>
2022-10-21 03:37:33 +0300

    Create html page by using javascript

--- script.js
+++ script.js
@@ -14,50 +14,123 @@ document.querySelectorAll('.menu').forEach((n) => n.addEventListener('click', ()
   mobileMenu.classList.remove('open-mobileMenu');
 }));
 
-const btn1 = document.getElementById('modalbtn1');
-const modal1 = document.getElementById('modal1');
-const closemodal1 = document.getElementById('closemodal1');
+const projects = [
+  {
+    id: 'card1',
+    img: {
+      src: 'images/deskcard1.png',
+      alt: 'card image 1',
+    },
+    title: 'Tonic',
+    list: ['CANOPY', 'Back End Dev', '2015'],
+    description:
+      "Lorem Ipsum is simply dummy text of the printings and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+    languages: ['HTML', 'css', 'Ruby and rails', 'javascript'],
+  },
+  {
+    id: 'card2',
+    img: {
+      src: 'images/deskcard4.png',
+      alt: 'card image 2',
+    },
+    title: 'Multi-Post <br> Stories',
+    list: ['Canopy', 'Back End Dev', '2015'],
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+    languages: ['HTML', 'Ruby on rails', 'css', 'javascript'],
+  },
+  {
+    id: 'card3',
+    img: {
+      src: 'images/deskcard5.png',
+      alt: 'card images 3',
+    },
+    title: 'Facebook 360',
+    list: ['Canopy', 'Back End Dev', '2015'],
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.",
+    languages: ['HTML', 'Ruby on rails', 'css', 'javascript'],
+  },
+  {
+    id: 'card4',
+    img: {
+      src: 'images/deskcard8.png',
+      alt: 'card image 4',
+    },
+    title: 'Uber Navigation',
+    list: ['Canopy', 'Back End Dev', '2015'],
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent.",
+    languages: ['HTML', 'Ruby on rails', 'css', 'javascript'],
+  },
+];
 
-btn1.addEventListener('click', () => {
-  modal1.classList.add('openbtn1');
+const modalOverlay = document.querySelector('.modal-overlay');
+function createModal() {
+  projects.forEach((input) => {
+    modalOverlay.innerHTML += `
+    <div class="modal" aria-controls="${input.id}">
+      <h6>${input.title}</h6> 
+      <span class="close">&times;</span>
+      <ul class="modal-list">
+        <li id="canopy-modal">${input.list[0]}</li>
+        <li><img src="images/Canopy.png" alt="point image" /></li>
+        <li>${input.list[1]}</li>
+        <li><img src="images/Canopy.png" alt="point image" /></li>
+        <li>${input.list[2]}</li>
+      </ul>
+        <img class="modal-img" src="${input.img.src}" alt="${input.img.alt}" />
+        <div class="description-container">
+          <p class="modal-description">
+            ${input.description}
+          </p>
+          <div class="program-language">
+            <ul id="language-names">
+              <li>${input.languages[0]}</li>
+              <li>${input.languages[1]}</li>
+              <li>${input.languages[2]}</li>
+              <li>${input.languages[3]}</li>
+            </ul>
+            <hr>
+            <div class="modal-btns">
+              <button id="seeLive" href="">
+              See Live
+              <img src="images/see-live.png" alt="Live img" />
+              </button>
+              <button href="">
+              See Source
+              <img src="images/see-source.png" alt="Github logo" />
+              </button>
+            </div>
+          </div>
+        </div>
+      
+    </div> `;
+  });
+}
+createModal();
+
+const popupOverlay = document.querySelector('.modal-overlay');
+const seeProjects = document.querySelectorAll('.see-projects');
+const modal = popupOverlay.querySelectorAll('.modal');
+
+seeProjects.forEach((card) => {
+  card.onclick = () => {
+    popupOverlay.style.display = 'flex';
+
+    const link = card.getAttribute('data-card');
+    modal.forEach((s) => {
+      const button = s.getAttribute('aria-controls');
+      if (link === button) {
+        s.classList.add('active');
+      }
+    });
+  };
 });
 
-closemodal1.addEventListener('click', () => {
-  modal1.classList.remove('openbtn1');
-});
-
-const btn2 = document.getElementById('modalbtn2');
-const modal2 = document.getElementById('modal2');
-const closemodal2 = document.getElementById('closemodal2');
-
-btn2.addEventListener('click', () => {
-  modal2.classList.add('openbtn1');
-});
-
-closemodal2.addEventListener('click', () => {
-  modal2.classList.remove('openbtn1');
-});
-
-const btn3 = document.getElementById('modalbtn3');
-const modal3 = document.getElementById('modal3');
-const closemodal3 = document.getElementById('closemodal3');
-
-btn3.addEventListener('click', () => {
-  modal3.classList.add('openbtn1');
-});
-
-closemodal3.addEventListener('click', () => {
-  modal3.classList.remove('openbtn1');
-});
-
-const btn4 = document.getElementById('modalbtn4');
-const modal4 = document.getElementById('modal4');
-const closemodal4 = document.getElementById('closemodal4');
-
-btn4.addEventListener('click', () => {
-  modal4.classList.add('openbtn1');
-});
-
-closemodal4.addEventListener('click', () => {
-  modal4.classList.remove('openbtn1');
+modal.forEach((close) => {
+  close.querySelector('.close').onclick = () => {
+    close.classList.remove('active');
+    popupOverlay.style.display = 'none';
+  };
 });
