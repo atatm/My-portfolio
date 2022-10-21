e7bd1bf00f32811b084a28a89daaa24a59806a7b
atatm <alelignayana@gmail.com>
2022-10-21 11:15:31 +0300

    Validate form using javascript

--- script.js
+++ script.js
@@ -140,12 +140,12 @@ const email = document.getElementById('mail');
 const showerror = document.getElementById('error');
 
 form.addEventListener('submit', (func) => {
-  const message = 'Email must be a Lowercase,you include un upperCase letter';
-  if (email.value.toLowerCase() === email.value) {
-    form.submit();
-  } else {
+  const errormessage = 'Email must be a Lowercase,you include un upperCase letter';
+  if (email.value.toLowerCase() !== email.value) {
     func.preventDefault();
-    showerror.innerText = message;
+    showerror.innerText = errormessage;
     showerror.style.opacity = 1;
+  } else {
+    form.submit();
   }
 });
